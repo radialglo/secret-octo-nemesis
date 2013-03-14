96919419b254cef8588965e84caf80762d778aa7
Anthony Su <antchovee@gmail.com>
2013-03-14 01:30:46 -0700

    added p2p.sh

---
+++ p2p.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ ./osppeer -dtest -t11112 walrus.jpg cat7.jpg cat6.jpg cat5.jpg cat4.jpg cat3.jpg cat2.jpg cat1.jpg about.txt walrus.jpg cat7.jpg cat6.jpg cat5.jpg cat4.jpg cat3.jpg cat2.jpg cat1.jpg about.txt walrus.jpg cat7.jpg cat6.jpg cat5.jpg cat4.jpg cat3.jpg cat2.jpg cat1.jpg about.txt
